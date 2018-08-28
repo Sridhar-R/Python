1976d6e3157a96d6b1099dec91b8424b88141b91
Sridhar-R <38914194+Sridhar-R@users.noreply.github.com>
2018-08-28 21:14:08 +0530

    Add files via upload

---
+++ prime_check.py
@@ -0,0 +1,6 @@
+num =input("Give the number that need to be check:")
+
+if (num % 2 == 0 ) or (num % 3 == 0) or (num % 5 == 0) or (num % 7 == 0):
+	print("the number is not prime")
+else:
+	print("the number is prime")
