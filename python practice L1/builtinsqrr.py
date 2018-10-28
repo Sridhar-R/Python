b33aa41bcdebfa36b7e0735a0808fc6e6e04cf56
Sridhar R <38914194+Sridhar-R@users.noreply.github.com>
2018-10-28 12:29:22 +0530

    Add files via upload

---
+++ python practice L1/builtinsqrr.py
@@ -0,0 +1,12 @@
+print (abs.__doc__)
+print (int.__doc__)
+print (input.__doc__)
+
+def square(num):
+	''' Return the squarre value of input number,
+	    the input number must be integer.
+	'''
+	return num**2
+print (square(2))
+print (square.__doc__)
+
